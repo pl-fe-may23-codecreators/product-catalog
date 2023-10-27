74bff4acc9d6c9f06e4e16beeba38bc46b758fee
Emil Owczarek <owczarekemil@gmail.com>
2023-10-27 23:39:37 +0200

    adding gh-pages, hash  instead of browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PhonesPage from './pages/PhonesPage';
 import ErrorPage from './pages/ErrorPage';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {path: '/', element: <HomePage />, errorElement: <ErrorPage />},
   {path: '/phones', element: <PhonesPage />}
 ]);
