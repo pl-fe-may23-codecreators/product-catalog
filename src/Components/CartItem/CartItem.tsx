6d56ad0304447fe8327bda5ae1afcdc0f2d21b9e
Klaudia <kklis@interia.pl>
2023-10-31 13:48:57 +0100

    Implement cart page, add layout and fix header icons

--- src/Components/CartItem/CartItem.tsx
+++ src/Components/CartItem/CartItem.tsx
@@ -1,23 +1,17 @@
-import { Phone } from '../../types/Phone';
 import './CartItem.scss';
-import phones from '../../api/phones.json';
 
-export const CartItem = () => {
-  const exampleItem: Phone = phones[0];
-
-  return (
-    <div className="CartItem">
-      <div className='CartItem__content'>
-        <button className='CartItem__remove'></button>
-        <img className='CartItem__image' src={require('../CardPhone/00.jpg')} alt="Cart item image" />
-        <div className="CartItem__name">{exampleItem.name}</div>
-        <div className="CartItem__count">
-          <button className="CartItem__count--button CartItem__count--decrease-amount" />
-          <div className="CartItem__count--amount">1</div>
-          <button className='CartItem__count--button CartItem__count--increase-amount' />
-        </div>
-        <div className="CartItem__price">${exampleItem.price}</div>
+export const CartItem = () => (
+  <div className="CartItem">
+    <div className='CartItem__content'>
+      <button className='CartItem__remove'></button>
+      <img className='CartItem__image' src={require('./example.png')} alt="Cart item image" />
+      <div className="CartItem__name">Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)</div>
+      <div className="CartItem__count">
+        <button className="CartItem__count--button CartItem__count--decrease-amount" />
+        <div className="CartItem__count--amount">1</div>
+        <button className='CartItem__count--button CartItem__count--increase-amount' />
       </div>
+      <div className="CartItem__price">$100</div>
     </div>
-  );
-};
+  </div>
+);
