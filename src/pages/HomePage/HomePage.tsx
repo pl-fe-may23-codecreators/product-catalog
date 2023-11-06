3a07c3926b62cc5d2862c31d15a35778f1450671
Emil Owczarek <owczarekemil@gmail.com>
2023-11-06 17:27:49 +0100

    slider changes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,13 +3,21 @@ import './HomePage.scss';
 import { RecommendedGoods } from '../../components/RecommendedGoods/RecommendedGoods';
 import { examplePhones } from '../../phonesFromServer/ExamplePhones';
 import { ShopByCategory } from '../../components/ShopByCategory/ShopByCateory';
-import { Slider } from '../../components/ImageSlider/Slider';
+import { Slider } from '../../components/Slider/Slider';
+import {SmallSlider} from '../../components/Slider/SmallSlider';
 
 const HomePage = () => {
   return (
     <div className="container">
       <h2 className="home-page__title">Welcome to Nice Gadgets store!!</h2>
-      <Slider />
+      
+      <div className="hide-on-mobile">
+        <Slider />
+      </div>
+      <div className="show-on-mobile">
+        <SmallSlider />
+      </div>
+
       <RecommendedGoods phones={examplePhones} title={'Brand new models'} />
       <ShopByCategory />
       <RecommendedGoods phones={examplePhones} title={'Hot prices'} />
